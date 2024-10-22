fbb4fe5f7bcacf1f919c15e69fc9d36cbb0b5696
Han KyungHun <hkh9601@naver.com>
2024-10-22 14:27:01 +0900

    feat : 이미지 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,28 @@
 export type { CustomError } from './error'
+
+export type RankTier =
+  | 'IRON'
+  | 'BRONZE'
+  | 'SILVER'
+  | 'GOLD'
+  | 'PLATINUM'
+  | 'DIAMOND'
+  | 'MASTER'
+  | 'GRANDMASTER'
+  | 'CHALLENGER'
+  | 'UNRANKED';
+
+export type TierImages = {
+  [key in RankTier]: string;
+};
+
+export type Images = TierImages & {
+  logo: string;
+  ICON: string;
+  CHAMPION: string;
+  ITEM: string;
+  SPELL: string;
+  RUNE: string;
+  BLUR: string;
+  DEFAULT: string;
+};
